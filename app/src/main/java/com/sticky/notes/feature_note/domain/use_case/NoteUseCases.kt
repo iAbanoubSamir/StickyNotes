f2c67cf700a4085e2767d6bf94247deebe7360ac
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-03-16 17:55:20 +0200

    Change it from sealed to data Class

--- app/src/main/java/com/sticky/notes/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/sticky/notes/feature_note/domain/use_case/NoteUseCases.kt
@@ -1,6 +1,6 @@
 package com.sticky.notes.feature_note.domain.use_case
 
-sealed class NoteUseCases(
+data class NoteUseCases(
     val getNotes: GetNotesUseCase,
     val deleteNote: DeleteNoteUseCase
 )
