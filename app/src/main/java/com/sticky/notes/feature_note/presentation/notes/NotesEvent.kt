0f0f534bba4c5a7a84fb1fc55520f68d79e51751
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-03-16 21:19:16 +0200

    Create NotesEvent Class

---
+++ app/src/main/java/com/sticky/notes/feature_note/presentation/notes/NotesEvent.kt
@@ -0,0 +1,11 @@
+package com.sticky.notes.feature_note.presentation.notes
+
+import com.sticky.notes.feature_note.domain.model.Note
+import com.sticky.notes.feature_note.domain.util.NoteOrder
+
+sealed class NotesEvent {
+    data class Order(val noteOrder: NoteOrder) : NotesEvent()
+    data class DeleteNote(val note: Note) : NotesEvent()
+    object RestoreNote : NotesEvent()
+    object ToggleOrderSection : NotesEvent()
+}
