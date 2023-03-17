ef074cccd691da159bb77ea5ff36d3858e203209
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-03-17 13:47:14 +0200

    Create Order Section UI

---
+++ app/src/main/java/com/sticky/notes/feature_note/presentation/notes/components/OrderSection.kt
@@ -0,0 +1,59 @@
+package com.sticky.notes.feature_note.presentation.notes.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.sticky.notes.feature_note.domain.util.NoteOrder
+import com.sticky.notes.feature_note.domain.util.OrderType
+
+@Composable
+fun OrderSection(
+    modifier: Modifier = Modifier,
+    noteOrder: NoteOrder = NoteOrder.Date(OrderType.DESC),
+    onOrderChange: (NoteOrder) -> Unit
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DefaultRadioButton(
+                text = "Title",
+                selected = noteOrder is NoteOrder.Title,
+                onSelect = { onOrderChange(NoteOrder.Title(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = "Date",
+                selected = noteOrder is NoteOrder.Date,
+                onSelect = { onOrderChange(NoteOrder.Date(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = "Color",
+                selected = noteOrder is NoteOrder.Color,
+                onSelect = { onOrderChange(NoteOrder.Color(noteOrder.orderType)) }
+            )
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DefaultRadioButton(
+                text = "Ascending",
+                selected = noteOrder.orderType is OrderType.ASC,
+                onSelect = { noteOrder.copy(orderType = OrderType.ASC) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = "Descending",
+                selected = noteOrder.orderType is OrderType.DESC,
+                onSelect = { noteOrder.copy(orderType = OrderType.DESC) }
+            )
+        }
+    }
+}
